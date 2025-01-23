e2f7ecbfcbfb89536f9b632b0148bc0c8504b67a
hamza wajeeh <hamzawajeeh80@gmail.com>
2025-01-24 01:07:37 +0500

    Initial commit of my Python project

---
+++ browser.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWebEngineWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.showMaximized()
